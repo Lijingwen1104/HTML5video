488c04751e16ec8ba74981629b88a876fddf9aa5
MsCtrl0 <940356316@qq.com>
2017-03-10 10:41:49 +0800

    添加 sass 编译

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 
 var gulp = require('gulp'),
-    connect=require("gulp-connect");
+    connect=require("gulp-connect"),
+    sass=require("gulp-sass");
 //起服务
 gulp.task('connect',function(){
     connect.server({
         port: 8001,
         livereload: true
-
     })
 }) 
 //刷新所有html
@@ -14,9 +14,17 @@ gulp.task('html',function(){
     gulp.src('*.html')
         .pipe(connect.reload());
 })
+//编译sass
+gulp.task('sass',function(){
+    gulp.watch(['static/sass/*.scss'],function(){
+        gulp.src('static/sass/*.scss')
+            .pipe(sass())
+            .pipe(gulp.dest('static/css'))
+    })
+})
 //监听所有html文件，改动了触发上一条任务
 gulp.task('watch',function(){
-    gulp.watch(['*.html',"css/*.css",'js/*.js'],['html'])
+    gulp.watch(['*.html',"static/css/*.css",'js/*.js'],['html','sass'])
 })
 //default默认起服务和监听
 gulp.task('default',['connect','watch'])
